d6088fe1af88b0a0b8f44238994bdebb6a2651f9
Fazil <fazil@mobil80.com>
2026-01-02 17:52:23 +0530

    Android

--- limewyre/android/app/build.gradle.kts
+++ limewyre/android/app/build.gradle.kts
@@ -1,4 +1,5 @@
 import java.util.Properties
+import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
@@ -7,10 +8,18 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+
 dependencies {
     implementation("com.google.errorprone:error_prone_annotations:2.23.0")
-    compileOnly("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("com.google.code.findbugs:jsr305:3.0.2")
 }
+
 
 
 kotlin {
@@ -45,26 +54,24 @@ android {
 
     signingConfigs {
     create("release") {
-        storeFile = file("limewyre.jks")
-        storePassword = System.getenv("STORE_PASSWORD")
-        keyAlias = "limewyre"
-        keyPassword = System.getenv("KEY_PASSWORD")
+        storeFile = file(keystoreProperties["storeFile"]!!)
+        storePassword = keystoreProperties["storePassword"] as String
+        keyAlias = keystoreProperties["keyAlias"] as String
+        keyPassword = keystoreProperties["keyPassword"] as String
     }
 }
 
-
-    buildTypes {
-    getByName("release") {
+buildTypes {
+    release {
+        signingConfig = signingConfigs.getByName("release")
         isMinifyEnabled = true
         isShrinkResources = true
         proguardFiles(
             getDefaultProguardFile("proguard-android-optimize.txt"),
             "proguard-rules.pro"
         )
-        signingConfig = signingConfigs.getByName("release")
     }
 }
-
 
 }
 
